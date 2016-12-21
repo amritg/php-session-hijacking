a5df7e6b0cff5cf93a410f8706977f7615b89328
Amrit Gautam <gautamamrit24@gmail.com>
2016-12-22 00:51:43 +0200

    Add file preActivateSessions_incomplete.php(Couldnot set the constant session_id for admin,amritg)

--- login-script.php
+++ login-script.php
@@ -1,6 +1,7 @@
 <?php
     require("config.php");
-    require("preActivateSessions_1.php");
+    // require("preActivateSessions_1.php");
+    require("preActivateSessions_2.php");
 
     if(isset($_POST["logIn"]) && !empty($_POST["logIn"])){
 
@@ -22,21 +23,21 @@
                 /*  1) ****** HexaDecimal encoded Session Cookie ID ****** */
                     // bin2hex() -> Returns the HexaDecimal representaion of the given String
                     // The line below sets the session cookie id to the encoded HexaDecimal value of the userName form the database. 
-                    $sessionId = session_id((bin2hex($logName)));
+                    // $sessionId = session_id((bin2hex($logName)));
  
                 /* 2) ****** Incrementing HexaDecimal encoded Session Cookie ID fetched from Database ***** */
-                // $getSessionQuery = "SELECT * FROM sessionId";
-                // $sessionArray = mysqli_query($conn,$getSessionQuery);
-                // $session = mysqli_fetch_array($sessionArray, MYSQLI_ASSOC);
+                $getSessionQuery = "SELECT * FROM sessionId";
+                $sessionArray = mysqli_query($conn,$getSessionQuery);
+                $session = mysqli_fetch_array($sessionArray, MYSQLI_ASSOC);
 
-                // $sessionNumber = $session['sessionNumber'];
-                // $sessionId = $session['id'];
-                // $newSessionNumber = $session['sessionNumber'] + 5;
+                $sessionNumber = $session['sessionNumber'];
+                $sessionId = $session['id'];
+                $newSessionNumber = $session['sessionNumber'] + 5;
 
-                // $updateSessionQuery = "UPDATE sessionId SET sessionNumber='$newSessionNumber' WHERE id='$sessionId'";
-                // mysqli_query($conn,$updateSessionQuery);
+                $updateSessionQuery = "UPDATE sessionId SET sessionNumber='$newSessionNumber' WHERE id='$sessionId'";
+                mysqli_query($conn,$updateSessionQuery);
 
-                // $sessionId = session_id(bin2hex($sessionNumber));
+                $sessionId = session_id(bin2hex($sessionNumber));
                 
                 session_start();
 
