b3a315cd286c55ec0bf39e6a198903567cb7e4f4
Amrit Gautam <gautamamrit24@gmail.com>
2016-12-27 04:08:58 +0200

    Add more descriptive comments in preActivateSessions.php

--- preActivateSessions_2.php
+++ preActivateSessions_2.php
@@ -1,25 +1,33 @@
-<?php 
+
+<?php
+    /* 
+        Everytime when any users login in successfully,
+        1. Loop through all the users, whose session is to be pre-activated, in order to Check if the session has already started.
+        2. If there is no pre-activated session, create session for each users by setting the session_id(), incrementing interger number by 5, fetched from db.
+    */
     require("config.php");
     $preActivateSessionUsers = ["amritg","admin","a"]; // Users whose session will start automatically
 
     for($i = 0; $i < (count($preActivateSessionUsers)); $i++){
         if(checkStoredSession($preActivateSessionUsers[$i]) == false){
+            // Create new session
             $sessionQuery = mysqli_query($conn,"SELECT * FROM sessionId");
             $session = mysqli_fetch_array($sessionQuery, MYSQLI_ASSOC);
             $sessionId = $session['id'];
-            
             $sessionNumber = $session['sessionNumber'];
             // echo "Session Number is: $sessionNumber"."<br>";
-            $newSessionNumber = $session['sessionNumber'] + 5;
-            $updateSessionQuery = "UPDATE sessionId SET sessionNumber='$newSessionNumber' WHERE id='$sessionId'";
-            mysqli_query($conn,$updateSessionQuery);
-            
+
             session_id($sessionNumber);
             // echo "Session id is:". session_id() . "<br>";
             session_start();
             $_SESSION['userName'] = $preActivateSessionUsers[$i];
             // echo "User Created: ".$_SESSION['userName'] . "<br>";
             session_write_close();
+
+            // Update database sessionNumber by 5 for new session
+            $newSessionNumber = $session['sessionNumber'] + 5;
+            $updateSessionQuery = "UPDATE sessionId SET sessionNumber='$newSessionNumber' WHERE id='$sessionId'";
+            mysqli_query($conn,$updateSessionQuery);
         }
     } 
     // chechStoredSession function checks if the session has already be set or not for Users where $sessionVariable(userName) is to be set automatically!! 
